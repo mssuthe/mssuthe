b08ca987e4c2978803756a43bd7d0d3f14a47bb5
unknown <mssuthe@MPL1DEVDSK001.polarisdev.com>
2013-05-20 15:09:58 -0500

    initial

---
+++ HelloWorldBuildTest/HelloWorldBuildTest/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HelloWorldBuildTest.Models;
+
+namespace HelloWorldBuildTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //
+        // GET: /Home/
+
+        public ActionResult Index()
+        {
+            HomeModel home = new HomeModel();
+            return View(home);
+        }
+
+        [HttpPost]
+        public ActionResult Index(HomeModel home, string name)
+        {
+            home.name = name;
+            home.GetMessage();
+            return View(home);
+        }
+        
+    }
+}
---
+++ HelloWorldBuildTest/HelloWorldBuildTest/Models/HomeModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace HelloWorldBuildTest.Models
+{
+    public class HomeModel
+    {
+        public string name;
+        public DateTime date;
+        public string message;
+
+        public void GetMessage()
+        {
+            date = DateTime.Now;
+            message = "Hello " + name + ", the date is " + date;
+        }
+    }
+}
